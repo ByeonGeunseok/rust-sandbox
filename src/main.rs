e9bc166155adaaa39b8bc59203faf530168424f5
Geunseok Byeon <byeongeunseok@gmail.com>
2023-07-06 19:55:23 +0900

    check a print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// "cargo run"
 fn main() {
+    // Basic print macro
     println!("Hello, world!");
+
+    // Print with arguments
+    println!("Arg1:{}, arg2:{}, arg3:{}, arg4:{}", "ONE", "two", 'C', 4);
+
+    // This Todo! macro will return panic message.
+    todo!("This is TODO panic message.");
 }
