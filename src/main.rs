a1b5817842293d5e58f91f1f3e0e9b5e1478c74b
Geunseok Byeon <byeongeunseok@gmail.com>
2023-07-06 20:20:53 +0900

    check variables

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 // "cargo run"
 fn main() {
+    let a_number = 10;
+    let a_word = "TEN";
+    let mut re_number = 11;
+    let mut re_word = "ELEVEN";
+
+    println!("The number: {}", a_number);
+    println!("The word: {}", a_word);
+
+    println!("Check a mutable value: {}, {}", re_number, re_word);
+    re_number = 9;
+    re_word = "NINE";
+    println!("Check a mutable value: {}, {}", re_number, re_word);
+
+    check_print();
+}
+
+fn check_print() {
     // Basic print macro
     println!("Hello, world!");
 
