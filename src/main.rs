880c9031b281460942c7dc2a3fa98b13cd5d09b5
Geunseok Byeon <byeongeunseok@gmail.com>
2023-07-07 22:45:21 +0900

    practice with vectors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 // "cargo run"
 fn main() {
+    println!("-*- -*- -*- -*- -*- -*- -*-");
     practice_array();
+    println!("-*- -*- -*- -*- -*- -*- -*-");
+    practice_vector();
 }
 
 fn practice_array() {
@@ -12,4 +15,32 @@ fn practice_array() {
 
     println!("{:?}", arr_init);
     println!("SUN,{},TUE,WED,{},FRI,SAT", monday, thursday);
+}
+
+fn practice_vector() {
+    let three_nums = vec![1, 2, 3];
+    let zeros = vec![0; 5];
+
+    println!("{:?}", three_nums);
+    println!("{:?}", zeros);
+
+    let mut alphabets = Vec::new();
+
+    alphabets.push("A");
+    alphabets.push("B");
+    println!("{:?}", alphabets);
+
+    alphabets.push("D");
+    println!("{:?}", alphabets);
+
+    println!("{:?}", alphabets.pop());
+
+    alphabets.push("C");
+    println!("{:?}", alphabets);
+
+    alphabets[2] = "c";
+    println!("{:?}", alphabets);
+
+    let beyond = alphabets[4];
+    println!("{}", beyond);
 }
