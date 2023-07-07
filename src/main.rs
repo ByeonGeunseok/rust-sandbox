179c5978d4a72bc713627bee0b3033772b14e189
Geunseok Byeon <byeongeunseok@gmail.com>
2023-07-07 21:27:03 +0900

    check enum variants

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 fn main() {
     println!("==========");
     println!("==========");
-
     println!("==========");
     println!("==========");
     check_var();
     check_shadowing();
     check_data_type();
     check_structure();
+    check_enum_variants();
     check_print();
 }
 
@@ -114,4 +114,34 @@ fn check_structure() {
         "first: {}, second: {}, third: {}",
         tuple_e.0, tuple_e.1, tuple_e.2
     );
+}
+
+fn check_enum_variants() {
+    #[derive(Debug)]
+    struct KeyPress(String, char);
+
+    #[derive(Debug)]
+    struct MouseClick {
+        x: i64,
+        y: i64,
+    }
+
+    #[derive(Debug)]
+    enum WebEvent {
+        WELoad(bool),
+        WEClick(MouseClick),
+        WEKeys(KeyPress),
+    }
+
+        let click = MouseClick { x: 100, y: 250 };
+        println!("Mouse click location: {}, {}", click.x, click.y);
+
+        let keys = KeyPress(String::from("Ctrl +"), 'N');
+        println!("\nKeys pressed: {}{}", keys.0, keys.1);
+
+        let we_load = WebEvent::WELoad(true);
+        let we_click = WebEvent::WEClick(click);
+        let we_key = WebEvent::WEKeys(keys);
+
+        println!("\nWebEvent enum structure: \n\n {:#?} \n\n {:#?} \n\n {:#?}", we_load, we_click, we_key);
 }
