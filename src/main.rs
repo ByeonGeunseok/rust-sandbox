8b943a02a086b95536bfd1381bd5f185518a5a16
Geunseok Byeon <byeongeunseok@gmail.com>
2023-07-07 22:10:58 +0900

    finish the intro. Hello, Rust!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,43 @@
 // "cargo run"
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+#[derive(PartialEq, Debug)]
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+    Car {
+        color,
+        transmission,
+        convertible,
+        mileage: 0,
+    }
+}
+
 fn main() {
-    
+    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    println!(
+        "Car1 = {}, {:?} transmission, convertible: {}, mileage: {}",
+        car.color, car.transmission, car.convertible, car.mileage
+    );
+
+    car = car_factory(String::from("Black"), Transmission::Automatic, true);
+    println!(
+        "Car2 = {}, {:?} transmission, convertible: {}, mileage: {}",
+        car.color, car.transmission, car.convertible, car.mileage
+    );
+
+    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, true);
+    println!(
+        "Car3 = {}, {:?} transmission, convertible: {}, mileage: {}",
+        car.color, car.transmission, car.convertible, car.mileage
+    );
 }
