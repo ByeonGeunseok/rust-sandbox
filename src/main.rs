080a4e281c2989a37cd2fd00ca95c154d7ab1642
Geunseok Byeon <byeongeunseok@gmail.com>
2023-07-12 19:28:24 +0900

    Practice with more data types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 // "cargo run"
 fn main() {
     println!("-*- -*- -*- -*- -*- -*- -*-");
-    practice_array();
+    practice_more_types();
     println!("-*- -*- -*- -*- -*- -*- -*-");
+    practice_array();
     practice_vector();
 }
 
@@ -43,4 +44,67 @@ fn practice_vector() {
 
     let beyond = alphabets[4];
     println!("{}", beyond);
+}
+
+fn practice_more_types() {
+    #[derive(PartialEq, Debug)]
+    struct Car {
+        color: String,
+        motor: Transmission,
+        roof: bool,
+        age: (Age, u32),
+    }
+
+    #[derive(PartialEq, Debug)]
+    enum Transmission {
+        Manual,
+        SemiAuto,
+        Automatic,
+    }
+
+    #[derive(PartialEq, Debug)]
+    enum Age {
+        New,
+        Used,
+    }
+
+    fn car_quality(miles: u32) -> (Age, u32) {
+        let quality: (Age, u32) = (Age::New, miles);
+
+        quality
+    }
+
+    fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
+        Car {
+            color: color,
+            motor: motor,
+            roof: roof,
+            age: car_quality(miles),
+        }
+    }
+
+    let colors = ["Blue", "Green", "Red", "Silver"];
+
+    let mut car: Car;
+    let mut engine = Transmission::Manual;
+
+    car = car_factory(String::from(colors[0]), engine, true, 0);
+    println!(
+        "Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    engine = Transmission::SemiAuto;
+    car = car_factory(String::from(colors[1]), engine, false, 100);
+    println!(
+        "Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    engine = Transmission::Automatic;
+    car = car_factory(String::from(colors[2]), engine, true, 200);
+    println!(
+        "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
 }
