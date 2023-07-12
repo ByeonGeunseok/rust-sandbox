f53c1e0b1827ffe5a23308b01b8052e6a6f52ead
Geunseok Byeon <byeongeunseok@gmail.com>
2023-07-12 19:50:42 +0900

    Practice with if-else

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 // "cargo run"
 fn main() {
     println!("-*- -*- -*- -*- -*- -*- -*-");
-    practice_more_types();
+    practice_if_else();
     println!("-*- -*- -*- -*- -*- -*- -*-");
     practice_array();
     practice_vector();
+    practice_more_types();
 }
 
 fn practice_array() {
@@ -107,4 +108,80 @@ fn practice_more_types() {
         "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles",
         car.age.0, car.roof, car.motor, car.color, car.age.1
     );
+}
+
+fn practice_if_else() {
+    let is_t = true;
+    let greeting = if is_t { "RUST" } else { "rust" };
+
+    println!("{}", greeting);
+
+    #[derive(PartialEq, Debug)]
+    struct Car {
+        color: String,
+        motor: Transmission,
+        roof: bool,
+        age: (Age, u32),
+    }
+
+    #[derive(PartialEq, Debug)]
+    enum Transmission {
+        Manual,
+        SemiAuto,
+        Automatic,
+    }
+
+    #[derive(PartialEq, Debug)]
+    enum Age {
+        New,
+        Used,
+    }
+
+    fn car_quality(miles: u32) -> (Age, u32) {
+        if miles > 0 {
+            return (Age::Used, miles);
+        }
+        (Age::New, miles)
+    }
+
+    fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
+        if car_quality(miles).0 == Age::Used {
+            if roof {
+                println!(
+                    "Preparing a used car: {:?}, {}, Hard top, {} miles",
+                    motor, color, miles
+                );
+            } else {
+                println!(
+                    "Preparing a used car: {:?}, {}, Convertible, {} miles",
+                    motor, color, miles
+                );
+            }
+        } else {
+            if roof {
+                println!(
+                    "Building a new car: {:?}, {}, Hard top, {} miles",
+                    motor, color, miles
+                );
+            } else {
+                println!(
+                    "Building a new car: {:?}, {}, Convertible, {} miles",
+                    motor, color, miles
+                );
+            }
+        }
+
+        Car {
+            color,
+            motor,
+            roof,
+            age: car_quality(miles),
+        }
+    }
+
+    car_factory(String::from("Orange"), Transmission::Manual, true, 0);
+
+    car_factory(String::from("Red"), Transmission::SemiAuto, false, 565);
+
+    car_factory(String::from("White"), Transmission::Automatic, true, 3000);
 }
