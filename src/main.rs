7aa6817bcb306af9e88c08f708807c9e6547c7a7
Geunseok Byeon <byeongeunseok@gmail.com>
2023-07-13 20:12:39 +0900

    Practice with hash map

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 // "cargo run"
 fn main() {
     println!("-*- -*- -*- -*- -*- -*- -*-");
-    practice_if_else();
+    practice_hash_map();
     println!("-*- -*- -*- -*- -*- -*- -*-");
     practice_array();
     practice_vector();
     practice_more_types();
+    practice_if_else();
 }
 
 fn practice_array() {
@@ -184,4 +185,95 @@ fn practice_if_else() {
     car_factory(String::from("Red"), Transmission::SemiAuto, false, 565);
 
     car_factory(String::from("White"), Transmission::Automatic, true, 3000);
+}
+
+fn practice_hash_map() {
+    #[derive(PartialEq, Debug)]
+    struct Car {
+        color: String,
+        motor: Transmission,
+        roof: bool,
+        age: (Age, u32),
+    }
+
+    #[derive(PartialEq, Debug)]
+    enum Transmission {
+        Manual,
+        SemiAuto,
+        Automatic,
+    }
+
+    #[derive(PartialEq, Debug)]
+    enum Age {
+        New,
+        Used,
+    }
+
+    fn car_quality(miles: u32) -> (Age, u32) {
+        if miles > 0 {
+            return (Age::Used, miles);
+        }
+
+        (Age::New, miles)
+    }
+
+    fn car_factory(order: i32, miles: u32) -> Car {
+        let colors = ["Blue", "Green", "Red", "Silver"];
+
+        let mut color = order as usize;
+        if color > 4 {
+            color = color - 4;
+        }
+
+        let mut motor = Transmission::Manual;
+        let mut roof = true;
+        if order % 3 == 0 {
+            motor = Transmission::Automatic;
+        } else if order % 2 == 0 {
+            motor = Transmission::SemiAuto;
+            roof = false;
+        }
+
+        Car {
+            color: String::from(colors[(color - 1) as usize]),
+            motor: motor,
+            roof: roof,
+            age: car_quality(miles),
+        }
+    }
+
+    use std::collections::HashMap;
+    let mut orders: HashMap<i32, Car> = HashMap::new();
+
+    let mut order = 1;
+    let mut car: Car;
+
+    car = car_factory(order, 1000);
+    orders.insert(order, car);
+    println!("car order:{}, {:?}", order, orders.get(&order));
+
+    order = order + 1;
+    car = car_factory(order, 2000);
+    orders.insert(order, car);
+    println!("car order:{}, {:?}", order, orders.get(&order));
+
+    order = order + 1;
+    car = car_factory(order, 0);
+    orders.insert(order, car);
+    println!("car order:{}, {:?}", order, orders.get(&order));
+
+    order = order + 1;
+    car = car_factory(order, 0);
+    orders.insert(order, car);
+    println!("car order:{}, {:?}", order, orders.get(&order));
+
+    order = order + 1;
+    car = car_factory(order, 3000);
+    orders.insert(order, car);
+    println!("car order:{}, {:?}", order, orders.get(&order));
+
+    order = order + 1;
+    car = car_factory(order, 4000);
+    orders.insert(order, car);
+    println!("car order:{}, {:?}", order, orders.get(&order));
 }
