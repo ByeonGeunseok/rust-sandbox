c2ec5048e8cdc18e6bfdd84eac4fd7be5bf8226c
Geunseok Byeon <byeongeunseok@gmail.com>
2023-07-15 12:30:37 +0900

    Practice with Option

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 // "cargo run"
 fn main() {
     println!("-*- -*- -*- -*- -*- -*- -*-");
-    practice_loop();
+    practice_option();
     println!("-*- -*- -*- -*- -*- -*- -*-");
+    practice_loop();
 }
 
 fn practice_loop() {
@@ -78,4 +79,47 @@ fn practice_loop() {
             miles = miles + 700;
         }
     }
+}
+
+fn practice_option() {
+    struct Person {
+        first: String,
+        middle: Option<String>,
+        last: String,
+    }
+
+    fn build_full_name(person: &Person) -> String {
+        let mut full_name = String::new();
+        full_name.push_str(&person.first);
+        full_name.push_str(" ");
+
+        if let Some(middle) = &person.middle {
+            full_name.push_str(&middle);
+            full_name.push_str(" ")
+        }
+
+        full_name.push_str(&person.last);
+        full_name
+    }
+
+    let john = Person {
+        first: String::from("James"),
+        middle: Some(String::from("Oliver")),
+        last: String::from("Smith"),
+    };
+    assert_eq!(build_full_name(&john), "James Oliver Smith");
+
+    let alice = Person {
+        first: String::from("Alice"),
+        middle: None,
+        last: String::from("Stevens"),
+    };
+    assert_eq!(build_full_name(&alice), "Alice Stevens");
+
+    let bob = Person {
+        first: String::from("Robert"),
+        middle: Some(String::from("Murdock")),
+        last: String::from("Jones"),
+    };
+    assert_eq!(build_full_name(&bob), "Robert Murdock Jones");
 }
