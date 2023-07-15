0ece2af54d1e02539af52d2def95cd342a5e349a
Geunseok Byeon <byeongeunseok@gmail.com>
2023-07-15 12:48:07 +0900

    Practice with Result

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // "cargo run"
 fn main() {
     println!("-*- -*- -*- -*- -*- -*- -*-");
-    practice_option();
+    practice_result();
     println!("-*- -*- -*- -*- -*- -*- -*-");
     practice_loop();
+    practice_option();
 }
 
 fn practice_loop() {
@@ -122,4 +123,33 @@ fn practice_option() {
         last: String::from("Jones"),
     };
     assert_eq!(build_full_name(&bob), "Robert Murdock Jones");
+}
+
+fn practice_result() {
+    use std::fs::File;
+    use std::io::{Error, Read};
+    use std::path::PathBuf;
+
+    fn read_file_contents(path: PathBuf) -> Result<String, Error> {
+        let mut string = String::new();
+
+        let mut file: File = match File::open(path) {
+            Ok(file_handle) => file_handle,
+            Err(io_error) => return Err(io_error),
+        };
+
+        match file.read_to_string(&mut string) {
+            Ok(_) => (),
+            Err(io_error) => return Err(io_error),
+        };
+
+        Ok(string)
+    }
+
+    if read_file_contents(PathBuf::from("src/main.rs")).is_ok() {
+        println!("The program found the main file.");
+    }
+    if read_file_contents(PathBuf::from("non-existent-file.txt")).is_err() {
+        println!("The program reported an error for the file that doesn't exist.");
+    }
 }
